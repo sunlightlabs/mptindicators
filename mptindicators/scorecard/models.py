187a098af675916d062a4a9f005378dff342e3fc
Jeremy Carbaugh <jcarbaugh@gmail.com>
2015-04-06 14:20:29 -0400

    PEP 8 fixes

--- mptindicators/scorecard/models.py
+++ mptindicators/scorecard/models.py
@@ -2,70 +2,70 @@ from django.db import models
 
 
 class Region(models.Model):
-	name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
 
-	class Meta:
-		ordering = ('name',)
+    class Meta:
+        ordering = ('name',)
 
-	def __unicode__(self):
-		return self.name
+    def __unicode__(self):
+        return self.name
 
 
 class Country(models.Model):
-	slug = models.SlugField()
-	name = models.CharField(max_length=255)
-	region = models.ForeignKey(Region, related_name='countries', blank=True, null=True)
+    slug = models.SlugField()
+    name = models.CharField(max_length=255)
+    region = models.ForeignKey(Region, related_name='countries', blank=True, null=True)
 
-	class Meta:
-		ordering = ('name',)
-		verbose_name_plural = "Countries"
+    class Meta:
+        ordering = ('name',)
+        verbose_name_plural = "Countries"
 
-	def __unicode__(self):
-		return self.name
+    def __unicode__(self):
+        return self.name
 
 
 class Section(models.Model):
-	number = models.PositiveSmallIntegerField(default=0)
-	name = models.CharField(max_length=255)
+    number = models.PositiveSmallIntegerField(default=0)
+    name = models.CharField(max_length=255)
 
-	class Meta:
-		ordering = ('number',)
+    class Meta:
+        ordering = ('number',)
 
-	def __unicode__(self):
-		return self.name
+    def __unicode__(self):
+        return self.name
 
 
 class Subsection(models.Model):
-	section = models.ForeignKey(Section, related_name="subsections")
-	number = models.PositiveSmallIntegerField(default=0)
-	name = models.CharField(max_length=255)
+    section = models.ForeignKey(Section, related_name="subsections")
+    number = models.PositiveSmallIntegerField(default=0)
+    name = models.CharField(max_length=255)
 
-	class Meta:
-		ordering = ('section__number', 'number')
+    class Meta:
+        ordering = ('section__number', 'number')
 
-	def __unicode__(self):
-		return '{} / {}'.format(self.section.name, self.name)
+    def __unicode__(self):
+        return '{} / {}'.format(self.section.name, self.name)
 
 
 class Indicator(models.Model):
-	subsection = models.ForeignKey(Subsection, related_name='indicators')
-	number = models.PositiveSmallIntegerField(default=0)
-	name = models.TextField()
-	description = models.TextField(blank=True)
-	comment = models.TextField(blank=True)
-	references = models.TextField(blank=True)
+    subsection = models.ForeignKey(Subsection, related_name='indicators')
+    number = models.PositiveSmallIntegerField(default=0)
+    name = models.TextField()
+    description = models.TextField(blank=True)
+    comment = models.TextField(blank=True)
+    references = models.TextField(blank=True)
 
-	class Meta:
-		ordering = ('number',)
+    class Meta:
+        ordering = ('number',)
 
-	def __unicode__(self):
-		return self.name
-		
+    def __unicode__(self):
+        return self.name
+
 
 class IndicatorScore(models.Model):
-	indicator = models.ForeignKey(Indicator, related_name="indicator_scores")
-	country = models.ForeignKey(Country, related_name="indicator_scores")
-	score = models.PositiveSmallIntegerField(blank=True, null=True)
+    indicator = models.ForeignKey(Indicator, related_name="indicator_scores")
+    country = models.ForeignKey(Country, related_name="indicator_scores")
+    score = models.PositiveSmallIntegerField(blank=True, null=True)
 
-	class Meta:
-		ordering = ('country__name', 'indicator__number')
+    class Meta:
+        ordering = ('country__name', 'indicator__number')
