2f85055209cf5c869c0f02732a5cb0e0dd4547af
Jeremy Carbaugh <jcarbaugh@gmail.com>
2015-05-28 16:13:00 -0400

    make it all work

--- mptindicators/settings.py
+++ mptindicators/settings.py
@@ -36,8 +36,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mptindicators',
     'mptindicators.scorecard',
-    'debug_toolbar',
+    # 'debug_toolbar',
     'djangobower',
     'grunt',
 )
@@ -85,6 +86,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
